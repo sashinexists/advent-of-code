14d3b028b45697adfdc38fbd03fb3f22a463949c
Sashin Exists <myself@sashinexists.com>
2018-03-10 18:04:27 +1100

    Completed day 21

--- Day 21/script.js
+++ Day 21/script.js
@@ -1,7 +1,47 @@
 function init(input) {
     var fractal = ".#./..#/###";
     var fractalArr = fractal.split("/");
-    return fractalArr;
+    var splitArr;
+    for (var i = 0; i<18	; i++) {
+    	if (fractalArr[0].length % 2 === 0) {
+    		splitArr = split(fractalArr, 2);
+    		fractalArr = join(enhanceAll(splitArr, input));
+    	} else if (fractalArr[0].length % 3 === 0) {
+    		splitArr = split(fractalArr, 3);
+    		fractalArr = join(enhanceAll(splitArr, input));
+    	}
+
+    }
+    return onPixels(fractalArr.join("/"));
+}
+
+function onPixels(fractalString) {
+	var pixels = 0;
+	for (var i = 0; i<fractalString.length; i++) {
+		if(fractalString[i]==="#") {
+			pixels++
+		}
+	}
+	return pixels;
+}
+
+function enhanceAll(splitFractal, input) {
+	var output = [];
+	splitFractal.forEach(function(square) {
+		output.push(enhance(square, input));
+	})
+	return output;
+}
+
+function enhance(square, input) {
+	var squareString = square.join("/");
+	for (var i = 0; i<input.length; i++) {
+		if(inArray(allPermutations(square),input[i][0])) {
+			return input[i][1].split("/");
+		}
+	}
+	alert("Fatal error");
+	return square;
 }
 
 function split(fractal, divisor) {
@@ -19,19 +59,20 @@ function split(fractal, divisor) {
     return output;
 }
 
+//if there is an issue it is probably here
 function join(splitFractal) {
     var output = [];
-    var divisor = splitFractal.length;
-    var side = Math.sqrt(divisor);
+    var divisor = splitFractal[0].length;
+    var side = Math.ceil(Math.sqrt(splitFractal.length));
     var row;
-    for (var i = 0; i<splitFractal.length; i++) {
-        for(var j = 0; j<side; j+=divisor) {
-            row = "";
-            for (var k = 0; k<side; k++) {
-                row += splitFractal[k][j];
-            }
-            output.push(row);
-        }
+    for (var i = 0; i<splitFractal.length; i+=side) {
+    	for (var j = 0; j<splitFractal[i].length; j++) {
+    		row = "";
+    		for (var k = 0; k<side; k++) {
+    			row += splitFractal[i+k][j];
+    		}
+    		output.push(row);
+    	}
     }
     return output;
 }
@@ -47,7 +88,7 @@ function allPermutations(fractal) {
         output.push(fractal.join("/"));
         output.push(flipped.join("/"));
     }
-    return output;
+    return uniqueArray(output);
 }
 
 function rotate(fractal) {
@@ -102,17 +143,3 @@ function inArray(arr, a) {
     }
     return false;
 }
-
-//console.log(split(["###...","###...","###...","###..."],2));
-//console.log(join(split(["###...","###...","###...","###..."],2)));
-console.log(split(["##.","##.","##."],3));
-console.log(join(split(["##.","##.","##."],3)));
-/*
-function test(fractal) {
-    var permutations = uniqueArray(allPermutations(fractal));
-    permutations.forEach(function(permutation) {
-        console.log(printFractal(permutation.split("/")));
-    })
-};
-
-test(["##..","##..","##..","##.."]);*/
